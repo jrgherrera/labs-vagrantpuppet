6ed1b3435ecbe0529569b02f686cf5fc4098a571
Ronny <ronnyb@ronnyb-thi-m.cmass.criticalmass.com>
2013-08-13 16:38:51 -0600

    add vagrant project

--- manifests/default.pp
+++ manifests/default.pp
@@ -19,5 +19,4 @@ class { 'mysql' :
 	password => "root",
 }
 
-class { 'php' :
-}
+class { 'php' : }
--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,28 +1,19 @@
-class php () {
-	package { 'apache2' :
+class php {
+	$packages = [
+		"php5",
+		"php5-cli",
+		"php5-curl",
+		"php5-dev",
+		"php5-gd",
+		"php5-geoip",
+		"php5-mcrypt",
+		"php5-mysql",
+		"php5-uuid",
+		"libapache2-mod-php5",
+	]
+
+	package { $packages :
 		ensure  => 'present',
 		require => Exec["apt-get update"],
-	}
-
-	service { 'apache2' :
-		ensure     => 'running',
-		enable     => true,
-		hasrestart => true,
-		require    => Package["apache2"],
-		subscribe  => [
-			File['apache-virtualhosts'],
-			Exec['apache-servername'],
-		],
-	}
-
-	file { 'apache-virtualhosts' :
-		path    => '/etc/apache2/sites-enabled/000-default',
-		source  => '/vagrant/modules/apache/files/apache-virtualhosts',
-		require => Package['apache2'],
-	}
-
-	exec { 'apache-servername' :
-		command => "echo \"ServerName ${servername}\" | sudo tee /etc/apache2/conf.d/fqdn",
-		require => Package["apache2"],
 	}
 }
